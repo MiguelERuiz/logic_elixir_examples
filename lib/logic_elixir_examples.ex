76bb08ec671f0a826e14ef11e88389d86f13d577
Miguel E. Ruiz <meruiz77@gmail.com>
2023-07-08 21:08:15 +0200

    New examples of using LogicElixir

--- lib/logic_elixir_examples.ex
+++ lib/logic_elixir_examples.ex
@@ -12,6 +12,24 @@ defmodule LogicElixirExamples do
   defpred character(:lisa)
   defpred character(:maggie)
 
+  defpred father_of(:homer, :bart)
+  defpred father_of(:homer, :lisa)
+  defpred father_of(:homer, :maggie)
+
+  defpred mother_of(:marge, :bart)
+  defpred mother_of(:marge, :lisa)
+  defpred mother_of(:marge, :maggie)
+
+  defpred siblings(X, Y) do
+    character(X)
+    character(Y)
+    father_of(Z, X)
+    father_of(Z, Y)
+    mother_of(T, X)
+    mother_of(T, Y)
+    @(X != Y)
+  end
+
   def characters do
     (findall X, do: character(X)) |> Enum.into([])
   end
@@ -19,4 +37,9 @@ defmodule LogicElixirExamples do
   def characters_count do
     (findall X, do: character(X)) |> Enum.count
   end
+
+  def siblings?(x, y) do
+    siblings({:ground, x}, {:ground, y}).(%{}) |> Enum.into([]) == [%{}]
+  end
+
 end
